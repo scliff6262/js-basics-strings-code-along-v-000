b277ab6e27a60f017895a3eccf10a735d4982e18
Sean Clifford <sclifford6262@gmail.com>
2018-05-02 14:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your code in this file!
 const currentUser = "Sean";
 
-let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
-let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const
